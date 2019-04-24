14842cd80857fa679e455c47baff1bf5a688e45e
Ugwuanyi <denisu29@gmail.com>
2019-04-24 17:20:46 +0100

    more story.html edits

--- zaraStories/static/star.js
+++ zaraStories/static/star.js
@@ -1,6 +1,18 @@
-$('.autor').each(function () {
-  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
-}).on('input', function () {
-  this.style.height = 'auto';
-  this.style.height = (this.scrollHeight) + 'px';
-});
+document.getElementById('shareBtn').onclick = function() {
+  FB.ui({
+    client_id:'327939761408427',
+    display: 'popup',
+    method: 'share',
+    href: '{{ request.build_absolute_uri }}{{ object.get_absolute_url }}',
+  }, function(response){});
+}
+
+
+
+(function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
