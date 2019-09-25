5f5b2b455846ee8810e0c4a900aa9ccfb76f85ad
Ugwuanyi Uchechukwu <denisu29@gmail.com>
2019-09-25 14:47:32 +0000

    modified .gitignore file

--- zaraStories/settings.py
+++ zaraStories/settings.py
@@ -31,7 +31,6 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'django_social_share',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
